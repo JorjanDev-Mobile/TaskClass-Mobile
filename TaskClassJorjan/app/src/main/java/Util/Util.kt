271d6b403aa2bd31b711a3d3bd5dda86b57f27f6
Jorjan <joalvarezal@est.utn.ac.cr>
2025-10-22 16:32:19 -0600

    Commit 2
    individual project preview 2

---
+++ TaskClassJorjan/app/src/main/java/Util/Util.kt
@@ -0,0 +1,15 @@
+package Util
+
+import android.content.Context
+import android.content.Intent
+
+class Util {
+
+    companion object{
+        fun OpenActivity(context: Context, objClassActivity: Class<*>){
+            val objIntent = Intent(context, objClassActivity)
+            context.startActivity(objIntent)
+        }
+
+    }
+}
